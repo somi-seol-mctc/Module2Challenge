f325acddadfa43349569404d8f97cfbf4d8922ef
somi-seol-mctc <tl3222zx@go.minnstate.edu>
2025-01-22 00:19:47 +0000

    initial commit

---
+++ Module2Challenge/Program.cs
@@ -0,0 +1,16 @@
+﻿float miles;
+float gallons;
+float mpg;
+
+///prompt user
+Console.Write("Enter number of miles driven: ");
+miles = float.Parse(Console.ReadLine());
+
+Console.Write("Enter number of gasoline used (gallons): ");
+gallons = float.Parse(Console.ReadLine());
+
+///calculate mpg
+mpg = miles / gallons;
+
+///display results
+Console.WriteLine($@"Your mpg is {mpg}");
